e3d8012fcff7df1d73961a042df53cd83cf3bfb3
Devin <dheadrick1618@gmail.com>
2023-01-08 02:25:09 -0700

    init commit

---
+++ catalog/views.py
@@ -0,0 +1,58 @@
+from django.views import generic
+from django.shortcuts import render
+from .models import Book, Author, BookInstance, Genre, Language
+
+# Can link a index page to any other page with <a href="{% url 'index' %}">Home</a>.  Using a reversed URL mapping
+
+# Create your views here.
+# Views process http requests, fetch req data from db, and render that data in the html page using html templates, and returns generated html in a http response to display to the user
+
+
+def index(request):
+    """View function for home page of site."""
+
+    # Generate counts of some of the main objects
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+
+    # Available books (status = 'a')
+    num_instances_available = BookInstance.objects.filter(
+        status__exact='a').count()
+
+    # The 'all()' is implied by default.
+    num_authors = Author.objects.count()
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
+
+
+class BookListView(generic.ListView):
+    model = Book
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+# Effecitvely above default class is this:
+
+# def book_detail_view(request, primary_key):
+#     try:
+#         book = Book.objects.get(pk=primary_key)
+#     except Book.DoesNotExist:
+#         raise Http404('Book does not exist')
+
+#     return render(request, 'catalog/book_detail.html', context={'book': book})
+
+# OR could do this:
+
+# from django.shortcuts import get_object_or_404
+# def book_detail_view(request, primary_key):
+#     book = get_object_or_404(Book, pk=primary_key)
+#     return render(request, 'catalog/book_detail.html', context={'book': book})
