75b0f8670da919c49efa0fd9a466437dcb6ef0fc
Devin <dheadrick1618@gmail.com>
2023-01-08 04:49:25 -0700

    added user auth and sessions counting via browker cookies key

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import generic
 from django.shortcuts import render
 from .models import Book, Author, BookInstance, Genre, Language
@@ -22,11 +23,15 @@ def index(request):
     # The 'all()' is implied by default.
     num_authors = Author.objects.count()
 
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
+        'num_visits': num_visits,
     }
 
     # Render the HTML template index.html with the data in the context variable
@@ -56,3 +61,13 @@ class BookDetailView(generic.DetailView):
 # def book_detail_view(request, primary_key):
 #     book = get_object_or_404(Book, pk=primary_key)
 #     return render(request, 'catalog/book_detail.html', context={'book': book})
+
+
+class LoanedBooksByUserListView(LoginRequiredMixin, generic.ListView):
+    """Generic class-based view listing books on loan to current user."""
+    model = BookInstance
+    template_name = 'catalog/bookinstance_list_borrowed_user.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
